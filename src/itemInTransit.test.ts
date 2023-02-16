4483bc7c18a0b0c16f3b6d60452560e576514f0e
goodyguts <adam.j.carruthers@hotmail.co.uk>
2023-02-16 09:31:00 +0530

    Make ItemInTransit simpler

--- src/itemInTransit.test.ts
+++ src/itemInTransit.test.ts
@@ -2,7 +2,7 @@ import { expect, test, jest } from "@jest/globals";
 import ItemInTransit from "./itemInTransit";
 
 test("passes time", () => {
-  const iit = new ItemInTransit(100, jest.fn(), jest.fn());
+  const iit = new ItemInTransit(100);
 
   expect(iit.getNextEventTime()).toEqual(100);
 
@@ -16,7 +16,7 @@ test("passes time", () => {
 });
 
 test("errors on passing too much time", () => {
-  const iit = new ItemInTransit(10, jest.fn(), jest.fn());
+  const iit = new ItemInTransit(10);
 
   iit.passTime(5);
   expect(() => iit.passTime(5.01)).toThrowError(
@@ -25,36 +25,15 @@ test("errors on passing too much time", () => {
 });
 
 test("doesn't error on passing exactly the right amount of time", () => {
-  const iit = new ItemInTransit(35, jest.fn(), jest.fn());
+  const iit = new ItemInTransit(35);
 
   iit.passTime(35);
 
   expect(iit.getNextEventTime()).toEqual(0);
 });
 
-test("update function is called with correct values", () => {
-  const updateMock = jest.fn();
-
-  const iit = new ItemInTransit(100, updateMock, jest.fn());
-
-  iit.updateGraphics();
-
-  expect(updateMock).toBeCalledTimes(1);
-  expect(updateMock).toBeCalledWith(100, 100);
-  updateMock.mockReset();
-
-  iit.passTime(10);
-  iit.updateGraphics();
-
-  expect(updateMock).toBeCalledTimes(1);
-  expect(updateMock).toBeCalledWith(90, 100);
-  updateMock.mockReset();
-});
-
 test("runEvent only executes at the correct time", () => {
-  const endMock = jest.fn();
-
-  const iit = new ItemInTransit(100, jest.fn(), endMock);
+  const iit = new ItemInTransit(100);
 
   expect(() => iit.runEvent()).toThrowError(
     "Event was run when the time left was not 0 (it was 100)"
@@ -63,6 +42,4 @@ test("runEvent only executes at the correct time", () => {
   iit.passTime(100);
 
   iit.runEvent();
-
-  expect(endMock).toBeCalledTimes(1);
 });
--- src/itemInTransit.ts
+++ src/itemInTransit.ts
@@ -1,17 +1,10 @@
-export type UpdateCb = (timeLeft: number, timeToDestination: number) => void;
-export type EndCb = () => void;
-
 export default class ItemInTransit {
   timeToDestination: number;
   timeLeft: number;
-  updateCb: UpdateCb;
-  endCb: EndCb;
 
-  constructor(timeToDestination: number, updateCb: UpdateCb, endCb: EndCb) {
+  constructor(timeToDestination: number) {
     this.timeToDestination = timeToDestination;
     this.timeLeft = timeToDestination;
-    this.updateCb = updateCb;
-    this.endCb = endCb;
   }
 
   getNextEventTime = () => {
@@ -27,16 +20,10 @@ export default class ItemInTransit {
     this.timeLeft -= dt;
   };
 
-  updateGraphics = () => {
-    this.updateCb(this.timeLeft, this.timeToDestination);
-  };
-
   runEvent = () => {
     if (this.timeLeft !== 0)
       throw new Error(
         `Event was run when the time left was not 0 (it was ${this.timeLeft})`
       );
-
-    this.endCb();
   };
 }
